a13971dc4c9282ee458e0ab095ee6d77a943857b
mlmedia <mike@millionlightsmedia.com>
2020-07-25 03:24:07 -0400

    - initial commit - pushing code copied from tutorial repo

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function (config) {
+	config.setBrowserSyncConfig({
+		https: {
+			key: '/etc/localhost.key',
+			cert: '/etc/localhost.crt'
+		}
+	});
+
+	config.addPassthroughCopy("src/js");
+	return {
+		dir: {
+			input: "src",
+			output: "dist",
+			data: "_data"
+		}
+	};
+};
