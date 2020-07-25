3413e6a403dcc2965b6debb8030372bca35c0a23
mlmedia <mike@millionlightsmedia.com>
2020-07-25 17:02:32 -0400

    - standardizing repo: README, gitignore, formatting

--- src/_data/news.js
+++ src/_data/news.js
@@ -4,23 +4,23 @@ const countries = require("./countries.json");
 require('dotenv').config();
 
 async function getNews(country) {
-  try {
-    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
-    return {
-     "country": country,
-     "articles": response.data.articles
-    }
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+		return {
+			"country": country,
+			"articles": response.data.articles
+		}
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 
-module.exports = async function() {
+module.exports = async function () {
 
-    var newsPromises = countries.map(getNews);
-    return Promise.all(newsPromises).then( newsObjects => {
-      return [].concat.apply([],newsObjects);
-    });
+	var newsPromises = countries.map(getNews);
+	return Promise.all(newsPromises).then(newsObjects => {
+		return [].concat.apply([], newsObjects);
+	});
 
 };
